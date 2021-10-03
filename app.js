15b49f3edccb2bd34c889b0cb4f53ccb3848bd53
whutkanido <flagranthusk@gmail.com>
2021-10-03 14:30:12 -0400

    4

---
+++ app.js
@@ -0,0 +1,75 @@
+// cache variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+// empty cart, ready to be filled with products
+let cart = [];
+
+
+// getting products from pedals.json
+class Products{
+ async getProducts(){
+     try {
+        let result = await fetch('pedals.json');
+        let data = await result.json();
+        let products = data.items;
+        products = products.map(item => {
+            const {title,price} = item.fields;
+            const {id} = item.sys;
+            const image = item.fields.image.fields.file.url;
+            return {title,price,id,image}
+        })
+        return products
+     } catch (error) {
+        console.log(error)
+     }
+  
+ }
+}
+
+// render products
+class UI{
+    displayProducts(products){
+        let result = '';
+        products.forEach(product =>{
+            result +=`
+            <!-- single product 40:28 -->
+            <article class="product">
+                <div class="img-container">
+                    <img src="${product.image}" class="product-img">
+                    <button class="bag-btn" data-id="${product.id}">
+                        <i class="fas fa-shopping-cart"></i>
+                        add to cart
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>
+            </article>
+            <!-- end single product -->
+            `;
+        });
+        productsDOM.innerHTML = result;
+    }
+
+}
+
+// local storage
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+const ui = new UI();
+const products = new Products();
+
+// get the products
+products.getProducts().then(products => ui.displayProducts(products));
+})
